913ecb7fc19f4cd0826506f36b2690749e3367cb
sikadm <33527388+sikadm@users.noreply.github.com>
2019-04-10 11:27:29 -0400

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,16 @@
+function remainingBalance() {
+    //grab interest rate
+    var r = document.getElementById("interest-input").value;
+    r = (r / 100) / 12;
+    //grab loan amount
+    var loan = document.getElementById("loan-input").value;
+    //grab number of payments
+    var payments = document.getElementById("payments-input").value;
+    //grab monthly payment
+    var monthly = document.getElementById("month-input").value;
+
+    var q = (1+r)** payments;
+    var remaining = q * loan - ( (q-1) / r ) * monthly;
+
+    document.getElementById("remaining-amount").innerHTML = remaining.toFixed(2);
+}
